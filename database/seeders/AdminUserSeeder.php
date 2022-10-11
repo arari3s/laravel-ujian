bc97eab7576b60dc593d114d962a34a2412498f7
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2022-10-11 19:58:59 +0700

    UPDATE: Update show profile and add seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Super Admin',
+                'email' => 'superadmin@laravelujian.test',
+                'password' => Hash::make('bismillah'),
+            ],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@laravelujian.test',
+                'password' => Hash::make('bismillah'),
+            ],
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
